77849c45dcb7ea237858ab12a0e0b4c322355ffb
Anirudh <anirudh012009@gmail.com>
2021-12-16 13:19:55 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+console.log("Hello World");
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+function add() {
+  const navMenu = document.querySelector("#nav-menu");
+  navMenu.classList.add("show-menu");
+}
+
+function remove() {
+  const navMenu = document.querySelector("#nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+/*==================== ACCORDION SKILLS ====================*/
+
+/*==================== QUALIFICATION TABS ====================*/
+
+/*==================== SERVICES MODAL ====================*/
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+/*==================== TESTIMONIAL ====================*/
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/
+
+/*==================== DARK LIGHT THEME ====================*/
